836a4704103a642b06de77fe3cce484de8f45bbc
MarioFLS <mariofernandolsantos@gmail.com>
2022-05-31 11:23:28 -0300

    feat: Toda vez que uma venda é criada, a quantidade do produto é modificada, assim tendo sempre o valor certo. Isso acontece também quando é editado e deletado. Sempre atribuindo a quantidade certa ao produto

--- models/ModelCreateSales.js
+++ models/ModelCreateSales.js
@@ -9,18 +9,24 @@ const createSales = async () => {
 };
 
 const createSalesProducts = async (salesId, arrBody) => {
-  const query = `INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) 
+  const queryCreateSales = `INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) 
   VALUES (?, ?, ?)`;
 
-  const sales = await Promise.all(arrBody.map(({ productId, quantity }) =>
-    connection.execute(query, [salesId, productId, quantity])));
+  const queryUpdate = `UPDATE StoreManager.products AS p, StoreManager.sales_products AS s
+  SET p.quantity = p.quantity - s.quantity`;
+
+  const sales = await Promise.all(arrBody.map(({ productId, quantity }) => {
+    connection.execute(queryCreateSales, [salesId, productId, quantity]); 
+    return connection.execute(queryUpdate);
+}));
   return sales;
 };
 
 const editSalesProducts = async (saleId, arrBody) => {
   const query = `UPDATE StoreManager.sales_products 
   SET product_id = ?, quantity = ?
-  WHERE sale_id = ? AND product_id = ? `;
+  WHERE sale_id = ? AND product_id = ?;
+  `;
 
   const sales = await Promise.allSettled(arrBody.map(({ productId, quantity }) =>
     connection.execute(query, [productId, quantity, saleId, productId])));
@@ -28,9 +34,13 @@ const editSalesProducts = async (saleId, arrBody) => {
 };
 
 const deleteSales = async (saleId) => {
-  const query = 'DELETE FROM StoreManager.sales_products WHERE sale_id = ?';
+  const queryUpdate = `UPDATE StoreManager.products AS p, StoreManager.sales_products AS s
+  SET p.quantity = p.quantity + s.quantity`;
+  
+  const queryDelete = 'DELETE FROM StoreManager.sales_products WHERE sale_id = ?';
 
-  const sales = await connection.execute(query, [saleId]);
+  await connection.execute(queryUpdate);
+  const sales = await connection.execute(queryDelete, [saleId]);
   return sales;
 };
 
