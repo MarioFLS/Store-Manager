da8d22eec4d421d5b2a64b0d5803f2c6d3b44f5a
MarioFLS <mariofernandolsantos@gmail.com>
2022-05-31 22:11:44 -0300

    test: Testes completos do arquivo de ServiceProducts

--- test/unit/services/ServiceProducts.js
+++ test/unit/services/ServiceProducts.js
@@ -9,16 +9,9 @@ describe("Testando Camada de Service - Products", () => {
     beforeEach(() => {
       const execute = [[
         {
-          "saleId": 1,
-          "date": "2021-09-09T04:54:29.000Z",
-          "productId": 1,
-          "quantity": 2
-        },
-        {
-          "saleId": 1,
-          "date": "2021-09-09T04:54:54.000Z",
-          "productId": 2,
-          "quantity": 2
+          "id": 2,
+          "name": "Traje de encolhimento",
+          "quantity": 20
         }
       ]];
       sinon.stub(connection, "execute").resolves(execute);
@@ -29,41 +22,13 @@ describe("Testando Camada de Service - Products", () => {
     });
 
     it("Retorna array com todos os itens", async () => {
-      const response = await ServiceStore.getProductsById(1);
+      const response = await ServiceStore.getProductsById(2);
 
       expect(response).to.be.a("array");
+      expect(response).to.deep.equal([{ id: 2, name: 'Traje de encolhimento', quantity: 20 }]);
     });
   });
 
-  describe('Retorna a busca do Produto pelo ID', () => {
-    beforeEach(() => {
-      const execute = [[
-        {
-          "id": 2,
-          "name": "Traje de encolhimento",
-          "quantity": 20
-        }
-      ]];
-      sinon.stub(connection, 'execute').resolves(execute)
-
-    });
-    afterEach(() => {
-      connection.execute.restore();
-    });
-    it('Query é a correta', async () => {
-      const id = 2
-      await ServiceStore.getProductsById(id);
-      const query = connection.execute.getCall(0);
-      expect(query.args).to.deep.equal(['SELECT * FROM StoreManager.products WHERE id = ?;', [2]]);
-    });
-
-    it('Produto buscado pelo ID', async () => {
-      const id = 2
-      const getProductsById = await ServiceStore.getProductsById(id);
-      expect(getProductsById).to.be.a('array');
-      expect(getProductsById).to.deep.equal([{ id: 2, name: 'Traje de encolhimento', quantity: 20 }]);
-    });
-  });
   describe('Caso não exista produto com o ID correspondente', () => {
     beforeEach(() => {
       const execute = [[]];
@@ -77,6 +42,126 @@ describe("Testando Camada de Service - Products", () => {
       const getProductsById = await ServiceStore.getProductsById(90);
       expect(getProductsById).to.deep
         .equal({ error: { message: 'Product not found', code: 404 } });
+    })
+  })
+});
+
+describe('Criação de Produto, Casos de sucesso', () => {
+  describe('Criando um Produto', () => {
+    beforeEach(() => {
+      const execute = [[{
+        "id": 2,
+        "name": "Traje de encolhimento",
+        "quantity": 20
+      }]];
+      sinon.stub(connection, 'execute').resolves(execute);
+    });
+    afterEach(() => {
+      connection.execute.restore();
+    });
+
+    it('Retorno Caso exista um Produto já com esse nome', async () => {
+      const nome = 'Traje de encolhimento';
+      const getProductsById = await ServiceStore.createProduct(nome, 2);
+      console.log(getProductsById);
+      expect(getProductsById).to.deep
+        .equal({ error: { message: 'Product already exists', code: 409 } });
+    })
+  });
+
+  describe('Editando um Produto', () => {
+    beforeEach(() => {
+      const execute = [[]];
+      sinon.stub(connection, 'execute').resolves(execute);
+    });
+    afterEach(() => {
+      connection.execute.restore();
+    });
+
+    it('Retorno Caso não exista um produto com esse ID', async () => {
+      const nome = 'Traje de encolhimento';
+      const getProductsById = await ServiceStore.editProduct(1, nome, 10);
+      console.log(getProductsById);
+      expect(getProductsById).to.deep
+        .equal({ error: {  message: 'Product not found', code: 404 } });
+    })
+  });
+
+  describe('Deletando um Produto', () => {
+    beforeEach(() => {
+      const execute = [[]];
+      sinon.stub(connection, 'execute').resolves(execute);
+    });
+    afterEach(() => {
+      connection.execute.restore();
+    });
+
+    it('Retorno Caso exista um Produto já com esse nome', async () => {
+      const getProductsById = await ServiceStore.deleteProduct(2);
+      console.log(getProductsById);
+      expect(getProductsById).to.deep
+        .equal({ error: {  message: 'Product not found', code: 404 } });
+    })
+  })
+});
+
+describe('Criação de Produto, Casos de erro', () => {
+  describe('Criando um Produto', () => {
+    beforeEach(() => {
+      const execute = [[]];
+      sinon.stub(connection, 'execute').resolves(execute);
+    });
+    afterEach(() => {
+      connection.execute.restore();
+    });
+
+    it('Retorno Caso exista um Produto já com esse nome', async () => {
+      const nome = 'Traje de encolhimento';
+      const getProductsById = await ServiceStore.createProduct(nome, 2);
+      console.log(getProductsById);
+      expect(getProductsById).to.be.a('array');
+    })
+  });
+
+  describe('Editando um Produto', () => {
+    beforeEach(() => {
+      const execute = [[{
+        "id": 2,
+        "name": "Traje de encolhimento",
+        "quantity": 20
+      }]];
+      sinon.stub(connection, 'execute').resolves(execute);
+    });
+    afterEach(() => {
+      connection.execute.restore();
+    });
+
+    it('Retorno Caso não exista um produto com esse ID', async () => {
+      const nome = 'Traje de encolhimento';
+      const getProductsById = await ServiceStore.editProduct(1, nome, 10);
+      expect(getProductsById).to.be.a('array');
+    })
+  });
+
+  describe('Deletando um Produto', () => {
+    beforeEach(() => {
+      const execute = [[
+        {
+          "id": 2,
+          "name": "Traje de encolhimento",
+          "quantity": 20
+        }
+      ]];
+      sinon.stub(connection, 'execute').resolves(execute);
+    });
+    afterEach(() => {
+      connection.execute.restore();
+    });
+
+    it('Retorno Caso exista um Produto já com esse nome', async () => {
+      const getProductsById = await ServiceStore.deleteProduct(2);
+      console.log(getProductsById);
+      expect(getProductsById).to.be.a('array');
     })
   })
 });
